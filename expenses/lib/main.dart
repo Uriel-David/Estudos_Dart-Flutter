7af308bf4dd3f453d689bb37eca7065f7a66cf8b
DESKTOP-QUL61PF\Uriel David <uriel.azevedo.ext@spms.min-saude.pt>
2022-12-04 19:36:02 +0000

    creation of project expenses app

---
+++ expenses/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ExpensesApp());
+}
+
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Personal Expenses'),
+      ),
+      body: const Center(
+        child: Text('Initial Version'),
+      ),
+    );
+  }
+}
