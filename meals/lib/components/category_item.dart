88048e39b866944751ac793e0f5544a23ee4a23f
DESKTOP-QUL61PF\Uriel David <uriel.azevedo.ext@spms.min-saude.pt>
2023-03-20 19:43:47 +0000

    creation base visual project and screens

---
+++ meals/lib/components/category_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem(this.category, {Key? key}) : super(key: key);
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+}
