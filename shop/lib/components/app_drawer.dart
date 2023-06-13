5b263434d8604dbcb2b18906aab9142b843b1e0c
Uriel-David <uriel.dqaa@gmail.com>
2023-06-13 18:24:06 +0100

    finish shop project - providers

---
+++ shop/lib/components/app_drawer.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:shop/utils/app_routes.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: const Text('Welcome, User!'),
+            automaticallyImplyLeading: false,
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.shop),
+            title: const Text('Store'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(
+                AppRoutes.home,
+              );
+            },
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.payment),
+            title: const Text('Orders'),
+            onTap: () {
+              Navigator.of(context).pushReplacementNamed(
+                AppRoutes.orders,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/models/cart.dart';
+import 'package:shop/models/order_list.dart';
 import 'package:shop/models/product_list.dart';
 import 'package:shop/pages/cart_page.dart';
+import 'package:shop/pages/orders_page.dart';
 import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/products_overview_page.dart';
 import 'package:shop/utils/app_routes.dart';
@@ -23,6 +25,9 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (_) => Cart(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => OrderList(),
         )
       ],
       child: MaterialApp(
@@ -34,11 +39,13 @@ class MyApp extends StatelessWidget {
           ),
           fontFamily: 'Lato',
         ),
-        home: const ProductsOverviewPage(),
+        // home: const ProductsOverviewPage(),
         debugShowCheckedModeBanner: false,
         routes: {
+          AppRoutes.home: (ctx) => const ProductsOverviewPage(),
           AppRoutes.productDetail: (ctx) => const ProductDetailPage(),
           AppRoutes.cart: (ctx) => const CartPage(),
+          AppRoutes.orders: (ctx) => const OrderPage(),
         },
       ),
     );
