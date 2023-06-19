35f6f22cb59495e2b4db6a4a6268ec3b0c1c2556
Uriel-David <uriel.dqaa@gmail.com>
2023-06-19 18:44:14 +0100

    finish creation form and validation

--- shop/lib/components/product_item.dart
+++ shop/lib/components/product_item.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
+import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
   const ProductItem(this.product, {super.key});
@@ -18,12 +21,45 @@ class ProductItem extends StatelessWidget {
         child: Row(
           children: <Widget>[
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  AppRoutes.product_form,
+                  arguments: product,
+                );
+              },
               icon: const Icon(Icons.edit),
               color: Theme.of(context).colorScheme.primary,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                showDialog<bool>(
+                  context: context,
+                  builder: (ctx) => AlertDialog(
+                    title: const Text('Certainty?'),
+                    content:
+                        const Text('Do you really want to delete this item?'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop(false);
+                        },
+                        child: const Text('No'),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(ctx).pop(true);
+                        },
+                        child: const Text('Yes'),
+                      ),
+                    ],
+                  ),
+                ).then((value) {
+                  if (value ?? false) {
+                    Provider.of<ProductList>(context, listen: false)
+                        .removeProduct(product);
+                  }
+                });
+              },
               icon: const Icon(Icons.delete),
               color: Theme.of(context).errorColor,
             ),
--- shop/lib/pages/product_form_page.dart
+++ shop/lib/pages/product_form_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 
 class ProductFormPage extends StatefulWidget {
   const ProductFormPage({super.key});
@@ -12,11 +15,33 @@ class _ProductFormPageState extends State<ProductFormPage> {
   final descriptionFocus = FocusNode();
   final imageUrlFocus = FocusNode();
   final imageUrlController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+  final formData = <String, Object>{};
 
   @override
   void initState() {
     super.initState();
     imageUrlFocus.addListener(updateImage);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (formData.isEmpty) {
+      final arg = ModalRoute.of(context)?.settings.arguments;
+
+      if (arg != null) {
+        final product = arg as Product;
+        formData['id'] = product.id;
+        formData['name'] = product.name;
+        formData['price'] = product.price;
+        formData['description'] = product.description;
+        formData['imageUrl'] = product.imageUrl;
+
+        imageUrlController.text = product.imageUrl;
+      }
+    }
   }
 
   @override
@@ -32,18 +57,48 @@ class _ProductFormPageState extends State<ProductFormPage> {
     setState(() {});
   }
 
+  void _submitForm() {
+    final isValid = formKey.currentState?.validate() ?? false;
+
+    if (!isValid) {
+      return;
+    }
+
+    formKey.currentState?.save();
+
+    Provider.of<ProductList>(context, listen: false).saveProduct(formData);
+    Navigator.of(context).pop();
+  }
+
+  bool isValidImageUrl(String url) {
+    bool isValidUrl = Uri.tryParse(url)?.hasAbsolutePath ?? false;
+    bool endsWithFile = url.toLowerCase().endsWith('.png') ||
+        url.toLowerCase().endsWith('.jpg') ||
+        url.endsWith('.jpeg');
+
+    return isValidUrl && endsWithFile;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Form Product'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: _submitForm,
+            icon: const Icon(Icons.save),
+          )
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(15),
         child: Form(
+          key: formKey,
           child: ListView(
             children: <Widget>[
               TextFormField(
+                initialValue: formData['name']?.toString(),
                 decoration: const InputDecoration(
                   labelText: 'Name',
                 ),
@@ -51,8 +106,23 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(priceFocus);
                 },
+                onSaved: (name) => formData['name'] = name ?? '',
+                validator: (paramName) {
+                  final name = paramName ?? '';
+
+                  if (name.trim().isEmpty) {
+                    return 'Name is required';
+                  }
+
+                  if (name.trim().length < 3) {
+                    return 'Name needs 3 letters at least.';
+                  }
+
+                  return null;
+                },
               ),
               TextFormField(
+                initialValue: formData['price']?.toString(),
                 decoration: const InputDecoration(
                   labelText: 'Price',
                 ),
@@ -64,8 +134,21 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(descriptionFocus);
                 },
+                onSaved: (price) =>
+                    formData['price'] = double.parse(price ?? '0'),
+                validator: (paramPrice) {
+                  final priceString = paramPrice ?? '-1';
+                  final price = double.tryParse(priceString) ?? -1;
+
+                  if (price <= 0) {
+                    return 'Price is required';
+                  }
+
+                  return null;
+                },
               ),
               TextFormField(
+                initialValue: formData['description']?.toString(),
                 decoration: const InputDecoration(
                   labelText: 'Description',
                 ),
@@ -75,6 +158,21 @@ class _ProductFormPageState extends State<ProductFormPage> {
                 maxLines: 3,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(imageUrlFocus);
+                },
+                onSaved: (description) =>
+                    formData['description'] = description ?? '',
+                validator: (paramDescription) {
+                  final description = paramDescription ?? '';
+
+                  if (description.trim().isEmpty) {
+                    return 'Description is required';
+                  }
+
+                  if (description.trim().length < 10) {
+                    return 'Name needs 10 letters at least.';
+                  }
+
+                  return null;
                 },
               ),
               Row(
@@ -89,6 +187,18 @@ class _ProductFormPageState extends State<ProductFormPage> {
                       focusNode: imageUrlFocus,
                       keyboardType: TextInputType.url,
                       controller: imageUrlController,
+                      onFieldSubmitted: (_) => _submitForm(),
+                      onSaved: (imageUrl) =>
+                          formData['imageUrl'] = imageUrl ?? '',
+                      validator: (paramImageUrl) {
+                        final imageUrl = paramImageUrl ?? '';
+
+                        if (!isValidImageUrl(imageUrl)) {
+                          return 'Please enter a valid URL.';
+                        }
+
+                        return null;
+                      },
                     ),
                   ),
                   Container(
