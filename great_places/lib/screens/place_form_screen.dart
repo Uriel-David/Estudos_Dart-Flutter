ccf4b4b70a11de4f0f2c53aec8a5325d42be3216
Uriel-David <uriel.dqaa@gmail.com>
2023-08-02 12:19:58 +0100

    feat: create initial struct to project

---
+++ great_places/lib/screens/place_form_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  const PlaceFormScreen({super.key});
+
+  @override
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('New Place'),
+      ),
+      body: const Center(
+        child: Text('Form Here!'),
+      ),
+    );
+  }
+}
---
+++ great_places/lib/screens/places_list.screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/utils/app_routes.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Locations'),
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.placeForm);
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
