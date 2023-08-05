2e89617160076580035687f69d7416988ccfaae5
Uriel-David <uriel.dqaa@gmail.com>
2023-08-05 20:44:19 +0100

    fix: adjust in main.dart add exception

--- great_places/lib/screens/places_list.screen.dart
+++ great_places/lib/screens/places_list.screen.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
 import 'package:flutter/material.dart';
 import 'package:great_places/providers/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
