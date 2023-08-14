81df7f2c0b919d6b3f2994e5e6e1ae3ac1474c98
Uriel-David <uriel.dqaa@gmail.com>
2023-08-14 12:38:19 +0100

    feat: initial commit and dotenv add in project

---
+++ chat/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat/pages/loading_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<void> main() async {
+  await dotenv.load();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoadingPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ chat/lib/pages/loading_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const CircularProgressIndicator(
+              backgroundColor: Colors.white,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Loading...',
+              style: TextStyle(
+                color: Theme.of(context).primaryTextTheme.headline6?.color,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
